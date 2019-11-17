4aa08dfc2f9f932df68f8034e812b8716c3d2ffd
knoll1572 <knoll1572@gmail.com>
2019-11-16 23:44:36 -0500

    Created project

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class TestClass {
     @Test
     public void Addition() {
-
+///test
         final int expected = 420;
         final int actual = Math.add(400, 20);
         Assert.assertEquals(actual, expected);
