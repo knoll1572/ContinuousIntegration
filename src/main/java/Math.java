167c1bdb7a7460e1c66a24981b40fb57827fddc2
knoll1572 <knoll1572@gmail.com>
2019-11-16 23:40:08 -0500

    Created project

---
+++ src/main/java/Math.java
@@ -0,0 +1,7 @@
+public class Math {
+    public static int add(int first, int second) {
+
+        return first + second;
+
+    }
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+    @Test
+    public void Addition() {
+
+        final int expected = 420;
+        final int actual = Math.add(400, 20);
+        Assert.assertEquals(actual, expected);
+    }
+}
